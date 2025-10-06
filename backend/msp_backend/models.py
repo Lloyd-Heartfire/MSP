2cf0977904aba53b4047aaad973c50db5f7ddebc
endle.amrane <endle.amrane@gmail.com>
2025-10-06 15:44:40 +0200

    delete constraint iso_code of location table

--- backend/msp_backend/models.py
+++ backend/msp_backend/models.py
@@ -195,7 +195,7 @@ class Location(models.Model):
         help_text="Subdivision administrative si disponible"
     )
     
-    admin2_USA = models.CharField(
+    admin2_usa = models.CharField(
         max_length=50,
         null=True,
         blank=True,
@@ -206,7 +206,6 @@ class Location(models.Model):
         max_length=10,
         null=True,
         blank=True,
-        unique=True,
         verbose_name="Code ISO"
     )
     
@@ -259,13 +258,6 @@ class Location(models.Model):
         indexes = [
             models.Index(fields=['country', 'province_state'], name='idx_location_hierarchy'),
             models.Index(fields=['who_region'], name='idx_who_region'),
-        ]
-        constraints = [
-            models.UniqueConstraint(
-                fields=['iso_code'], 
-                name='idx_iso_code',
-                condition=models.Q(iso_code__isnull=False)
-            ),
         ]
 
     def __str__(self):
