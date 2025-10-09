c3cbe42bc7ebeb93a8d56b8b93eeb6f2e0b3eaeb
Theosch16 <schmitt1690@gmail.com>
2025-10-09 15:28:27 +0200

    adding check_covid_data.py, get_csv.py, wrangling_covid.py

--- backend/etl/check_covid_data.py
+++ backend/etl/check_covid_data.py
@@ -3,48 +3,59 @@ import glob
 import os
 from datetime import datetime
 
-# Data range from March 1, 2020 to March 1, 2022
-start = datetime(2020, 3, 1)
-end = datetime(2022, 3, 1)
+def check_covid():
 
-# Final columns to keep
-final_columns = [
-    "Date","Admin2","Province_State","Country_Region","Confirmed","Deaths","Recovered","Active",
-]
+    # Data range from March 1, 2020 to March 1, 2022
+    start = datetime(2020, 3, 1)
+    end = datetime(2022, 3, 1)
 
-# List to hold individual DataFrames
-dfs = []
+    # Final columns to keep
+    final_columns = [
+        "observation_date","city","province_state","country","total_cases","total_deaths","total_recovered","active_cases",
+    ]
 
-# Loop through all CSV files in the directory
-for f in sorted(glob.glob("covid/csse_covid_19_data/csse_covid_19_daily_reports/*.csv")):
-    # Extract date from filename
-    filename = os.path.basename(f).replace(".csv", "")
-    # Parse date and check if within range
-    try:
-        date = datetime.strptime(filename, "%m-%d-%Y")
-        if start <= date <= end:
-            df_temp = pd.read_csv(f)
-            
-            # Standardize column names
-            df_temp = df_temp.rename(columns={
-                "Province/State": "Province_State",
-                "Country/Region": "Country_Region",
-            })
-            # Add date column
-            df_temp["Date"] = date
-            # Ensure all final columns are present
-            df_temp = df_temp.reindex(columns=final_columns)
-            # Append to list
-            dfs.append(df_temp)
-    except ValueError:
-        # Ignore files that don't match the date format
-        pass
+    # List to hold individual DataFrames
+    dfs = []
 
-# Concatenate all DataFrames
-df_final = pd.concat(dfs, ignore_index=True)
+    # Loop through all CSV files in the directory
+    for f in sorted(glob.glob("covid/csse_covid_19_data/csse_covid_19_daily_reports/*.csv")):
+        # Extract date from filename
+        filename = os.path.basename(f).replace(".csv", "")
+        # Parse date and check if within range
+        try:
+            date = datetime.strptime(filename, "%m-%d-%Y")
+            if start <= date <= end:
+                df_temp = pd.read_csv(f)
+                
+                # Standardize column names
+                df_temp = df_temp.rename(columns={
+                    "Province_State":"province_state",
+                    "Country_Region":"country",
+                    "Admin2":"city",
+                    "Province/State": "province_State",
+                    "Country/Region": "country",
+                    "Confirmed":"total_cases",
+                    "Deaths":"total_deaths",
+                    "Recovered":"total_recovered",
+                    "Active":"active_cases",
+                })
+                # Add date column
+                df_temp["observation_date"] = date
+                # Ensure all final columns are present
+                df_temp = df_temp.reindex(columns=final_columns)
+                # Append to list
+                dfs.append(df_temp)
+        except ValueError:
+            # Ignore files that don't match the date format
+            pass
 
-# Save the final CSV
-df_final.to_csv("covid_19.csv", index=False)
+    # Concatenate all DataFrames
+    df_final = pd.concat(dfs, ignore_index=True)
 
-print("CSV created : covid_19.csv")
+    # Save the final CSV
+    df_final.to_csv("covid_19.csv", index=False)
+
+    print("CSV created : covid_19.csv")
+    
+check_covid()
 
--- backend/etl/get_csv.py
+++ backend/etl/get_csv.py
@@ -3,44 +3,48 @@ import stat
 import subprocess
 import shutil
 
-# Variables
-repo_url = "https://github.com/CSSEGISandData/COVID-19.git"
-clone_dir = "/app/etl/covid"
-folder_to_get = "csse_covid_19_data/csse_covid_19_daily_reports"
+def get_covid():
 
-# Remove file if it already exist
-def remove_readonly(func, path, excinfo):
-    os.chmod(path, stat.S_IWRITE)
-    func(path)
+    # Variables
+    repo_url = "https://github.com/CSSEGISandData/COVID-19.git"
+    clone_dir = "/app/etl/covid"
+    folder_to_get = "csse_covid_19_data/csse_covid_19_daily_reports"
 
-if os.path.exists(clone_dir):
-    shutil.rmtree(clone_dir, onerror=remove_readonly)
+    # Remove file if it already exist
+    def remove_readonly(func, path, excinfo):
+        os.chmod(path, stat.S_IWRITE)
+        func(path)
 
-# Clone the repo with sparse checkout and filter blob none to only copy what interests us
-subprocess.run(f'git clone --depth 1 --filter=blob:none --sparse "{repo_url}" "{clone_dir}"',
-               shell=True, check=True)
+    if os.path.exists(clone_dir):
+        shutil.rmtree(clone_dir, onerror=remove_readonly)
 
-# Configure sparse-checkout to get only the folder we want
-subprocess.run(f'git sparse-checkout set "{folder_to_get}"',
-               shell=True, check=True, cwd=clone_dir)
+    # Clone the repo with sparse checkout and filter blob none to only copy what interests us
+    subprocess.run(f'git clone --depth 1 --filter=blob:none --sparse "{repo_url}" "{clone_dir}"',
+                shell=True, check=True)
 
-print(f"Dossier '{folder_to_get}' récupéré dans {clone_dir}")
+    # Configure sparse-checkout to get only the folder we want
+    subprocess.run(f'git sparse-checkout set "{folder_to_get}"',
+                shell=True, check=True, cwd=clone_dir)
 
-python_path = "python"
-script_path = "/app/etl/get_csv.py"
-log_path = "/app/get_csv.log"
+    print(f"Dossier '{folder_to_get}' récupéré dans {clone_dir}")
 
-# Every beginning of each month, we use a cron
-cron_line = f"0 0 1 * * {python_path} {script_path} >> {log_path} 2>&1"
+    python_path = "python"
+    script_path = "/app/etl/get_csv.py"
+    log_path = "/app/get_csv.log"
 
-# Recover existing crons
-result = subprocess.run(["crontab", "-l"], capture_output=True, text=True)
-existing_cron = result.stdout if result.returncode == 0 else ""
+    # Every beginning of each month, we use a cron
+    cron_line = f"0 0 1 * * {python_path} {script_path} >> {log_path} 2>&1"
 
-# Add a line in cron if it doesn't exist
-if cron_line not in existing_cron:
-    new_cron = existing_cron.strip() + "\n" + cron_line + "\n"
-    subprocess.run(["crontab", "-"], input=new_cron, text=True)
-    print("created")
-else:
-    print("La tâche cron existe déjà.")
+    # Recover existing crons
+    result = subprocess.run(["crontab", "-l"], capture_output=True, text=True)
+    existing_cron = result.stdout if result.returncode == 0 else ""
+
+    # Add a line in cron if it doesn't exist
+    if cron_line not in existing_cron:
+        new_cron = existing_cron.strip() + "\n" + cron_line + "\n"
+        subprocess.run(["crontab", "-"], input=new_cron, text=True)
+        print("created")
+    else:
+        print("La tâche cron existe déjà.")
+
+get_covid
