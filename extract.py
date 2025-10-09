919309aae16f5dca8d75b4ff098af341c97ae82e
Theosch16 <schmitt1690@gmail.com>
2025-10-09 08:30:20 +0200

    automation git covid data with cron

---
+++ extract.py
@@ -0,0 +1,46 @@
+import os
+import stat
+import subprocess
+import shutil
+
+# Variables
+repo_url = "https://github.com/CSSEGISandData/COVID-19.git"
+clone_dir = "/home/UIMM/project/MSP/Covid-19_github"
+folder_to_get = "csse_covid_19_data/csse_covid_19_daily_reports"
+
+# Remove file if it already doesn't exist
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+if os.path.exists(clone_dir):
+    shutil.rmtree(clone_dir, onerror=remove_readonly)
+
+# Clone the repo with sparse checkout and filter blob none to only copy what interests us
+subprocess.run(f'git clone --depth 1 --filter=blob:none --sparse "{repo_url}" "{clone_dir}"',
+               shell=True, check=True)
+
+# Configure sparse-checkout to get only the folder we want
+subprocess.run(f'git sparse-checkout set "{folder_to_get}"',
+               shell=True, check=True, cwd=clone_dir)
+
+print(f"Dossier '{folder_to_get}' récupéré dans {clone_dir}")
+
+python_path = "/usr/bin/python3"
+script_path = "/home/UIMM/project/MSP/extract.py"
+log_path = "/home/UIMM/project/MSP/extract.log"
+
+# Every beginning of each, we use a cron
+cron_line = f"0 0 1 * * {python_path} {script_path} >> {log_path} 2>&1"
+
+# Recover existing crons
+result = subprocess.run(["crontab", "-l"], capture_output=True, text=True)
+existing_cron = result.stdout if result.returncode == 0 else ""
+
+# Add a line in cron if it doesn't exist
+if cron_line not in existing_cron:
+    new_cron = existing_cron.strip() + "\n" + cron_line + "\n"
+    subprocess.run(["crontab", "-"], input=new_cron, text=True)
+    print("created")
+else:
+    print("La tâche cron existe déjà.")
