05ab3c756a2fb023045ca5bfbe1db8eb7f2ef21d
Lloyd-Heartfire <beaudonloic@hotmail.com>
2025-10-15 20:18:57 +0200

    Add missing button

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import Footer from '../Footer/Footer';
 import FilterPanel from '../FilterPanel/FilterPanel';
 import DatePicker from '../DatePicker/DatePicker';
 import ExportButton from '../ExportButton/ExportButton';
+import MetricChangeButton from '../MetricChangeButton/MetricChangeButton';
+import ValidateButton from '../ValidateButton/ValidateButton';
 import StatsCards from '../StatsCards/StatsCards';
 import Charts from '../Charts/Charts';
 import './Dashboard.css';
@@ -19,7 +21,7 @@ const Dashboard = () => {
         <div className="dashboard-container">
           {/* Titre principal */}
           <h1 className="dashboard-title">
-            Analysis of pandemics in the world
+            Analysis of the pandemics in the world
           </h1>
 
           {/* Section des filtres */}
@@ -27,9 +29,14 @@ const Dashboard = () => {
             <div className="filters-row">
               <FilterPanel />
             </div>
+          </section>
 
+          {/* Section des actions (Export, Toggle, Validate) */}
+          <section className="dashboard-section actions-section">
             <div className="actions-row">
               <ExportButton />
+              <MetricChangeButton />
+              <ValidateButton />
             </div>
           </section>
 
