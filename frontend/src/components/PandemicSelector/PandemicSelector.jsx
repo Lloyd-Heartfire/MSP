a91e6c8d0e09569594160e7cc40589c599ba0371
Lloyd-Heartfire <beaudonloic@hotmail.com>
2025-10-16 22:39:44 +0200

    Fixing minor CSS

--- frontend/src/components/PandemicSelector/PandemicSelector.jsx
+++ frontend/src/components/PandemicSelector/PandemicSelector.jsx
@@ -6,11 +6,6 @@ const PandemicSelector = () => {
     <div className="pandemic-selector">
       <div className="dropdown-button">
         <span className="pandemic-label">COVID-19</span>
-        <button className="pandemic-dropdown-btn">
-          <svg viewBox="0 0 24 24" width="16" height="16" fill="white">
-            <path d="M7 10l5 5 5-5z"/>
-          </svg>
-        </button>
       </div>
     </div>
   );
